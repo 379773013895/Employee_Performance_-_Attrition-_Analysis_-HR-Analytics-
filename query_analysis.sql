dce157c9aa484d1be4cc1ea7d2b5b76dbc0f2698
Mohini Bisen <117579161+379773013895@users.noreply.github.com>
2026-01-13 08:44:28 +0530

    Update query_analysis.sql

--- query_analysis.sql
+++ query_analysis.sql
@@ -34,3 +34,95 @@ FROM employees e
 JOIN performance_reviews pr ON e.employee_id = pr.employee_id
 JOIN salaries s ON e.employee_id = s.employee_id
 WHERE pr.performance_rating <= 2;
+
+--  Overall Attrition Rate
+SELECT 
+    ROUND(
+        SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS attrition_rate_percentage
+FROM employees;
+
+--  Department-wise Attrition Rate
+SELECT 
+    department,
+    COUNT(*) AS total_employees,
+    SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count,
+    ROUND(
+        SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS attrition_rate
+FROM employees
+GROUP BY department;
+
+-- 3Attrition by Job Role
+SELECT 
+    job_role,
+    COUNT(*) AS total_employees,
+    SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count
+FROM employees
+GROUP BY job_role
+ORDER BY attrition_count DESC;
+
+--  Average Performance Rating by Department
+SELECT 
+    department,
+    ROUND(AVG(performance_rating), 2) AS avg_performance_rating
+FROM employees
+GROUP BY department;
+
+--  Salary vs Attrition
+SELECT 
+    attrition,
+    ROUND(AVG(salary), 2) AS avg_salary
+FROM employees
+GROUP BY attrition;
+
+-- Experience Level Attrition Analysis
+SELECT 
+    experience_level,
+    COUNT(*) AS total_employees,
+    SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count
+FROM employees
+GROUP BY experience_level;
+
+-- Job Satisfaction vs Attrition
+SELECT 
+    job_satisfaction,
+    COUNT(*) AS total_employees,
+    SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count
+FROM employees
+GROUP BY job_satisfaction
+ORDER BY job_satisfaction;
+
+-- Top 5 Departments by Attrition Rate
+SELECT 
+    department,
+    ROUND(
+        SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS attrition_rate
+FROM employees
+GROUP BY department
+ORDER BY attrition_rate DESC
+LIMIT 5;
+
+-- High-Risk Employees (Low Performance + Low Satisfaction)
+SELECT 
+    employee_id,
+    department,
+    job_role,
+    performance_rating,
+    job_satisfaction
+FROM employees
+WHERE performance_rating <= 2
+  AND job_satisfaction <= 2;
+
+-- Department Performance Ranking (Window Function)
+SELECT 
+    department,
+    ROUND(AVG(performance_rating), 2) AS avg_rating,
+    RANK() OVER (ORDER BY AVG(performance_rating) DESC) AS performance_rank
+FROM employees
+GROUP BY department;
+
